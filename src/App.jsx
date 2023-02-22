d6776414fe23d2deb0a53301e5fd1f346f47f58c
Egor Vorobev <egorka.vorobev.94@mail.ru>
2023-02-23 00:20:04 +0300

    Redux (RTK) Category

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { Provider } from "react-redux";
+import { store } from "./store";
+
 import Header from "./components/Header/Header";
 import Navigation from "./components/Navigation/Navigation";
 import Catalog from "./components/Catalog/Catalog";
@@ -6,13 +9,13 @@ import Footer from "./components/Footer/Footer";
 export const App = () => {
 
   return (
-    <>
+    <Provider store={store}>
       <Header />
       <main>
         <Navigation />
         <Catalog />
       </main>
       <Footer />
-    </>
+    </Provider>
   );
 };
